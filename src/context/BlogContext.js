c1cb63d9cb9215c7ceaabac9727f878d680381f3
AdityaPandey03 <pandey.aditya4272@gmail.com>
2021-08-18 18:12:05 +0530

    adding Blogs done

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React,{ useReducer } from 'react'
+import createDataContext from './createDataContext'
 
-const BlogContext = React.createContext();
 
-export const BlogProvider = ({children}) =>{
+const blogReducer = (state,action) =>{
 
-    const blogPosts=[
-        {title:'Blog Post #1'},
-        {title:'Blog Post #2'}
-    ];
+    switch(action.type){
+        case 'add_blogpost':
+            return [...state,{title:`Blog Post #${state.length+1}`}]
+        default:
+            return state;
+    }
 
-    return <BlogContext.Provider value={blogPosts}>
-    {children}
-    </BlogContext.Provider>
-};
+}
 
-export default BlogContext;
+const addBlogPost = (dispatch) => {
+    return () =>{
+        dispatch({ type : 'add_blogpost'})
+    }
+  }
 
+
+export const { Context,Provider } = createDataContext(
+    blogReducer,
+    {addBlogPost},
+    []
+)
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,19 +1,23 @@
 import React , { useContext } from 'react';
-import {Text,View,Stylesheet,FlatList} from 'react-native';
-import BlogContext from '../context/BlogContext'; 
+import {Text,View,Stylesheet,FlatList,Button} from 'react-native';
+import { Context } from '../context/BlogContext'; 
+import {Feather} from '@expo/vector-icons'
 
 const IndexScreen = () =>{
 
-    const blogPosts= useContext(BlogContext);
+    const {state,addBlogPost}= useContext(Context);
 
     return (
         <View>
-            <Text>Hey this is Index Screen</Text>
+            <Button title="Add Post"  onPress={()=>addBlogPost()} />
             <FlatList 
-             data={blogPosts}/>
+             data={state}/>
              keyExtractor={(blogPost)=> blogPost.title }
              renderItem={({item}) => {
-                 return <Text>{item.title}</Text>
+                 return <View style={styles.row}>
+                 <Text style={styles.title}>{item.title}</Text>
+                 <Feather style={styles.icon} name="trash"/>
+                 </View>
              }}
 
             
@@ -21,6 +25,21 @@ const IndexScreen = () =>{
     )
 };
 
-const styles = StyleSheet.create({})  
+const styles = StyleSheet.create({
+    row:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth : 1,
+        borderColor : 'gray'
+    },
+    title:{
+        fontSize:20
+    },
+    icon:{
+        fontSize:   25
+    }
+})  
 
 export default IndexScreen;
